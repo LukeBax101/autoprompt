96d97c6a668bee14fc6304808766542ca5e120bc
Luke Baxter <luke.baxter@gbr.iae.net>
2021-05-05 00:54:43 +0100

    Updated public path

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: '/autoprompt/',
     filename: '[name].js'
   },
   target: 'web',
